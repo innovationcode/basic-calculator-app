87df455fa49d752b95574fc503e622ad537c4375
lajawanti <lajawanti.dhake@gmail.com>
2020-01-24 14:05:39 -0500

    basic rows for calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Calculator</h1>
-      
-    </div>
-  );
+class App extends React.Component{
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Calculator</h1>
+        <div>
+            {/* First Row  */}
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>+</li>
+            </ul>
+
+            {/* Second Row  */}
+            <ul>
+              <li>4</li>
+              <li>5</li>
+              <li>6</li>
+              <li>-</li>
+            </ul>
+
+            {/* Third Row  */}
+            <ul>
+              <li>7</li>
+              <li>8</li>
+              <li>9</li>
+              <li>*</li>
+            </ul>
+            
+            {/* Forth Row  */}
+            <ul>
+              <li>C</li>
+              <li>0</li>
+              <li>=</li>
+              <li>/</li>
+            </ul>
+        </div>
+      </div>
+    );
+  }  
 }
 
 export default App;
